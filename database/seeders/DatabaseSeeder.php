bc0e86c00b79de86b2ae3be00798d3abf9bef3ec
sarjid <sarjid777gmail.com>
2022-09-07 14:58:00 +0600

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::factory(10)->create();
+        \App\Models\Employee::factory(30)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+
+        User::create([
+            'name' => "Test",
+            'username' => "username",
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'), // password
+            'remember_token' => Str::random(10),
+        ]);
+    }
+}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,124 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css"
+        integrity="sha512-PgQMlq+nqFLV4ylk1gwUOgm6CtIIXkKwaIHp/PAIWHzig/lKZSEGKEysh0TCVbHJXCLN7WetD8TFecIky75ZfQ=="
+        crossorigin="anonymous" />
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                @guest
+                    @if (Route::has('login'))
+                        <a href="{{ route('login') }}" class="btn btn-outline rounded-pill">
+                            Login</a>
+                    @endif
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="btn btn-outline rounded-pill">
+                            Register</a>
+                    @endif
+                @else
+                    <a href="" class="btn btn-outline rounded-pill"> <i class="fa fa-users"></i>
+                        Employee</a>
+
+                @endguest
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        <!-- Authentication Links -->
+
+                        @auth
+
+
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle " href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item btn btn-outline  rounded-pill" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endauth
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+
+    <style>
+        .btn-outline {
+            border: 2px solid #008B8B;
+            color: #008B8B;
+            margin-right: 3px;
+            font-weight: bolder;
+        }
+
+        .btn-outline:hover {
+            background: #008B8B;
+            color: #fff;
+        }
+
+        /* fore delete button  */
+        .btn-outline-del {
+            border: 2px solid #E3342F;
+            color: #E3342F;
+            margin-right: 3px;
+            font-weight: bolder;
+        }
+
+        .btn-outline-del:hover {
+            background: #E3342F;
+            color: #fff;
+        }
+
+        .card-header {
+            background: #008B8B;
+            color: #fff;
+        }
+    </style>
+</body>
+
+</html>
